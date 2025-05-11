5368d662298aab159cf2b4ebaad9ebefe3ba162e
Amro <amrokahla@yahoo.com>
2025-05-11 12:45:04 +0300

    app test

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-import cv2
 import os
 import numpy as np
 from modules.face_mesh import FaceMeshDetector
 from modules.object_detector import ObjectDetector
 from modules.utils import COCO_OBJECTS
+import cv2
+
 
 # Set page config
 st.set_page_config(
